405af71817733d33af53a515014e8e47d800116b
Aaftab <pinjari.aaftab@gmail.com>
2021-08-23 14:14:48 +0530

    connected mongodb with the application

---
+++ backend/models/orderModel.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const orderSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    orderItems: [{
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Product'
+        },
+    }],
+    paassword: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+}, {
+    timestamps: true
+})
+
+const Order = mongoose.model('Order', orderSchema)
+
+export default Order;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,16 @@
 import express from 'express'
 
 import products from './data/products.js'
+import connectDB from './config/db.js'
 import dotenv from 'dotenv'
 
 const app = express()
 
 dotenv.config()
 
-const PORT = process.env.PORT
+connectDB()
+
+const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('API is running')
