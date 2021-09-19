0b9f50c9d62eb9f4709bb1d10b081766ab38b517
Aaftab <pinjari.aaftab@gmail.com>
2021-09-19 11:45:49 +0530

    done with user authentication and tokken generation

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import User from '../models/userModel.js'
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 
 //POST request to /api/users/login
 //desription auth user and get token
@@ -15,12 +16,31 @@ const authUser = asyncHandler(async (req, res) => {
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     } else {
         res.status(401)
         throw new Error("Invalid Email or Password")
     }
 })
+//GET request to /api/users/profile
+//desription auth user and get token
+//access Private
 
-export { authUser }
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error("User Not Found")
+    }
+})
+
+export { authUser, getUserProfile }
